768ef73447fd8540eac00e1f936d1e605da5d69f
Aemon33 <khazriaymen93@gmail.com>
2025-09-24 09:17:37 +0200

    final steps to api building

--- BACKEND/api/urls.py
+++ BACKEND/api/urls.py
@@ -4,8 +4,10 @@ from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
+
 router.register('project', ProjectViewset, basename='project')
-urlpatterns = router.urls()
+
+urlpatterns = router.urls
 
 # urlpatterns = [
 #     path('', home),
--- BACKEND/api/views.py
+++ BACKEND/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets, permissions
 from .models import *
+from .serializers import *
+from rest_framework.response import Response
 
 # NT 12:02
 
@@ -13,14 +15,34 @@ def home(request):
 class ProjectViewset(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
 
     def list(self, request):
-        pass
+        queryset  = self.queryset
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
     def create(self, request):
-        pass
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(): 
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
     def retrieve(self, request, pk=None):
-        pass
+        project = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(project)
+        return Response(serializer.data)
     def update(self, request, pk=None):
-        pass
+        project = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(project, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
     def destroy(self, request, pk=None):
-        pass
+        project = self.queryset.get(pk=pk)
+        project.delete()
+        return Response(status =204 )
