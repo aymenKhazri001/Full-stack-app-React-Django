374bf24f08f3f8243b6da6cfe8160db0aedb07a5
Aemon33 <khazriaymen93@gmail.com>
2025-09-24 08:44:06 +0200

    first steps of creating the api

--- BACKEND/api/views.py
+++ BACKEND/api/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from rest_framework import viewsets, permissions
+from .models import *
 
-
+# NT 12:02
 
 
 def home(request):
     return HttpResponse(f' this is the end {request.GET}')
+
+
+class ProjectViewset(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = Project.objects.all()
+
+    def list(self, request):
+        pass
+    def create(self, request):
+        pass
+    def retrieve(self, request, pk=None):
+        pass
+    def update(self, request, pk=None):
+        pass
+    def destroy(self, request, pk=None):
+        pass
