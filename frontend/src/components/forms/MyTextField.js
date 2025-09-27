8070829f58673671f4a849bde230a81befaa32c9
Aemon33 <khazriaymen93@gmail.com>
2025-09-27 16:25:45 +0200

    bring forms from mu and mui x

---
+++ frontend/src/components/forms/MyTextField.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+
+export default function MyTextField() {
+  return (
+    
+      
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+   
+  );
+}
